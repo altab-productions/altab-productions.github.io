b7c684dd5c841ec0fa0acaf0d2f0487b56aadffc
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-06 23:59:11 +0200

    Fixed the telegram link format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <h1>Welcome to Altab Productions!</h1>
 <p>This website is in active development.</p>
-<p>I have a <a href="t.me/altab_productions">telegram channel</a> where I write blogs about nutrition and (food) culture.</p>
+<p>I have a <a href="https://t.me/altab_productions">telegram channel</a> where I write blogs about nutrition and (food) culture.</p>
 <p>You can contact me through the following media:</p>
 <p>Email: <a href="mailto:business@altab.productions">business@altab.productions</a></p>
 <p>Phone: <a href="tel:+31642199246">+31 6 42 199 246</a> 10:00-12:00 Tue-Thu (local charges apply)</p>
