86b13d58bf8c56bfc6173fa9978f21e045f462a4
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 02:31:06 +0200

    Added a navigation header and about, contact, services pages

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,31 @@
+<div class="header--container">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/services">Services</a>
+    <a href="/contact">Contact</a>
+</div>
+
+<style>
+    .header--container {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        background-color: var(--turquoise--background);
+        height: fit-content;
+    }
+
+    a {
+        margin: 1rem;
+        margin-right: 3rem;
+        text-align: center;
+        vertical-align: middle;
+
+        font-family: Inter;
+        font-style: normal;
+        font-size: 2rem;
+        font-weight: 400;
+
+        color: white;
+    }
+</style>
