cb3d85efef200edb6f1b1f9f279ded53f351657b
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 04:19:31 +0200

    Created a footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,49 @@
+<div class="footer--container">
+    <section class="company-registry">
+        <p>Altab Productions EMZ</p>
+        <p>KvK number 94080046</p>
+        <p>VAT-ID number ...</p>
+        <a href="mailto:business@altab.productions">business@altab.productions</a>
+    </section>
+    <section class="company-details">
+        <p>Westerstraat 10, UNIT A4832</p>
+        <p>3016DH Rotterdam,</p>
+        <p>Zuid-Holland, Nederland</p>
+        <a href="tel:+31642199246">+31 6 42 199 246</a>
+    </section>
+</div>
+
+<style>
+    .footer--container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+
+        background-color: var(--turquoise--dark);
+    }
+
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        height: 100%;
+
+        margin: 0.5rem;
+        margin-bottom: 2rem;
+    }
+
+    section p {
+        width: fit-content;
+        margin: 0.2rem;
+        color: var(--gray--light);
+    }
+
+    section a {
+        width: fit-content;
+        margin: 0.2rem;
+
+        padding: 0 15px;
+    }
+</style>
