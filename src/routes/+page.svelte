dbcd459579da4849033b4e255507fa76417e75eb
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 04:20:36 +0200

    Updated main page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,23 @@
 <h1>Welcome to Altab Productions!</h1>
-<p>This website is in active development.</p>
-<p>I have a <a href="https://t.me/altab_productions">telegram channel</a> where I write blogs about nutrition and (food) culture.</p>
-<p>You can contact me through the following media:</p>
-<p>Email: <a href="mailto:business@altab.productions">business@altab.productions</a></p>
-<p>Phone: <a href="tel:+31642199246">+31 6 42 199 246</a> 10:00-12:00 Tue-Thu (local charges apply)</p>
+<p>
+    Altab Productions is a sole proprietorship owned by Maxim Istomin. Specializing in personalized and group consultations on nutrition and lifestyle choices, we are dedicated to helping you achieve optimal health and well-being. With a holistic approach, we tailor our system to your unique needs, offering expert advice and practical strategies to promote healthy living.
+</p>
+<h2>How it works</h2>
+<p>
+    The core of our strategy is a schedule of frequent and small meals, which are tuned to make you, and your body, feel full. Without feeling at danger of malnourishment, the body stops accumulating energy, allowing existing reserves to shrink. This makes this approach more healthy and sustainable, and therefore less prone to relapse.
+</p>
+<h2>Permissive diet</h2> 
+<p>
+    At the core of our approach is a permissive dieting strategy that does not impose exact meals but focuses on composition and appropriate times for consumption. This strategy ensures that nothing is off-limits for you: you are free to eat whatever you want! Instead, certain foods are recommended at specific intervals during the day to minimize their negative impact. For example, we advise to eat desserts earlier in the day, ideally no less than 30 but no more than 90 minutes after your second breakfast. This flexible approach allows you to enjoy a healthy diet without having to forgo your favorite foods.
+</p>
+<h2>Personalized advice</h2>
+<p>
+    Understanding that your body responds differently to dietary changes, Altab Productions provides personalized advice to tweak your diet composition based on your progress or lack thereof. During regular consultations, we will assess your results and make necessary adjustments. You might receive suggestions to reduce protein intake if it appears to be excessive, increase fiber to support digestive health, or replace specific elements if they hinder your progress. This tailored guidance ensures that you receive a customized plan that will let you achieve your ideal weight in spite of any hindrances.
+</p>
+<h2>
+    Crisis management
+</h2>
+<p>
+    Weight loss and lifestyle changes often come with emotional challenges, especially during periods of crisis when your body resists change to hold on to stored energy. We offer essential emotional support during these critical times. We provide empathetic counseling to help you navigate these rough patches, offering strategies to cope with cravings, stress, and the psychological aspects of dietary adjustments. This support is crucial in maintaining your motivation and preventing relapse, ensuring that you the goals that you set for yourself.
+</p>
+
