7467959b7829b31609ed7ddac37c5b742f537e73
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 04:45:39 +0200

    Expanded the contact page with details

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,4 @@
-Contact
+<h1>Contact</h1>
+<p>You can contact me through the following channels:</p>
+<p>Email: <a href="mailto:m.istomin@altab.productions">m.istomin@altab.productions</a></p>
+<p>Phone: <a href="tel:+31642199246">+31 6 42 199 246</a> 10:00-12:00 Tue-Thu (local charges apply)</p>
