bd0f614dbf77a844dc198c338cfd371144cdb1c5
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-27 01:03:58 +0200

    Updated the services page with a services breakdown

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -4,8 +4,22 @@
 
 <h1>Services</h1>
 <p>
-    Currently, all services are provided through the means of private communication. For details regarding pricing and getting started, please <a href="{base}/contact">contact me</a> directly.
-</p>
+    Currently, we provide the following services: <br>
+    <br>
+    <b>Club access</b><br>
+    <em>55 euros for 8 weeks</em><br>
+    With this, you get access to a private Telegram group chat, where you can ask questions about the system.<br>
+    <br>
+    <b>Group training sessions</b><br>
+    <em>100 euros per month</em><br>
+    With this, you get access to a private Telegram group chat, where you can ask questions about the system, and also educational group meetings which teach the details of the system.<br>
+    <br>
+    <b>Small group work</b><br>
+    <em>500 euros for the first 8 weeks, 400 for each subsequent renewal</em><br>
+    With this, you get access to a <em>separate</em> Telegram group chat, where more personalized questions and feedback could be exchanged. Additionally, you get access to recurring educational meetings about the details of the system, with personalized guidance to ensure your full understanding of metabolism and sustainable dieting.<br>
+    <br>
+    For further details about these services and instructions on getting started, please <a href="{base}/contact">contact me</a> directly.
+<p/>
 <h2>Mobile application</h2>
 <p>
     I am developing an application aimed at managing relevant elements of daily routine and having a more direct platform for providing my services. The app is not yet released, but you can follow my <a href="https://t.me/altab_productions">Telegram channel</a> for updates regarding progress and for functionality suggestions.
