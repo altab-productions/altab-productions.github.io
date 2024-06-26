b78204722764778f28d16f0b8a941d0c26ea4e36
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 04:46:23 +0200

    Added content to the services page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,1 +1,8 @@
-Services
+<h1>Services</h1>
+<p>
+    Currently, all services are provided through the means of private communication. For details regarding pricing and getting started, please <a href="/contact">contact me</a> directly.
+</p>
+<h2>Mobile application</h2>
+<p>
+    I am developing an application aimed at managing relevant elements of daily routine and having a more direct platform for providing my services. The app is not yet released, but you can follow my <a href="https://t.me/altab_productions">Telegram channel</a> for updates regarding progress and for functionality suggestions.
+</p>
