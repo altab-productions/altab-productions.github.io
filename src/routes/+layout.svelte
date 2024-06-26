b54aa507b7cd29c9daa6bda60af3d5da4f45b837
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 02:30:13 +0200

    Set up the font import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
-    // import "$lib/styles/reset.css";
+    import Header from "$lib/components/Header.svelte";
+    import "$lib/styles/reset.css";
     import "$lib/styles/colors.css";
+    import "$lib/styles/fonts.css";
 </script>
 
+<Header/>
 <slot/>
