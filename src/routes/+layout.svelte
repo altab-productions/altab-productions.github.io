fc33361bf20e831e60d4495d513006e29f571393
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-26 04:20:25 +0200

    updated root +layout to accomodate flex and footer positions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,34 @@
 <script>
     import Header from "$lib/components/Header.svelte";
+    import Footer from "$lib/components/Footer.svelte";
     import "$lib/styles/reset.css";
     import "$lib/styles/colors.css";
     import "$lib/styles/fonts.css";
+    import "$lib/styles/defaults.css";
 </script>
+<body>
+    <Header/>
+    <main>
+        <slot/>
+    </main>
+    <Footer/>
+</body>
 
-<Header/>
-<slot/>
+<style>
+    * {
+        font-family: 'Freigeist',Arial,sans-serif;
+    }
+
+    body {
+        display: flex;
+        flex-direction: column;
+
+        min-height: 100vh;
+        margin: 0;
+    }
+
+    main {
+        flex: 1;
+    }
+
+</style>
