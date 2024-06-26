2372620b91b1041152135378d52548560ebf43a4
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2024-06-27 01:09:11 +0200

    Added a terms of service page with introduction

---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,41 @@
+<h1>Terms of Service</h1>
+<p>
+    <em>
+        The present agreement is a legally binding agreement between you and Altab Productions. <br>
+        This is an important document which you should read carefully in order to make a decision on using services provided by Altab Productions. <br>
+        Read the present agreement carefully.
+        By signing up you confirm that you have read and understood the present agreement and agree to be bound by it.
+    </em>
+</p>
+<br>
+<p>
+    We have the right to amend and update the present Agreement, delete any terms hereof, and make additions and adopt new annexes hereto, make changes to the fees and other amounts charged related to your User Account, at any time, by publishing a new edition of the Agreement on the Website of Altab Productions at least 5 days prior to the moment the new edition comes into effect. We will inform you additionally about any change by sending the relevant notifications to you.
+</p>
+<br>
+<p>
+    Upon your reception of a notification of the changing of the Agreement, you shall read the new edition of the Agreement and accept the new conditions stipulated therein in order to continue your use of any of the Services provided by Altab Productions. In event of disagreement on the new edition of the Agreement or any of its parts, you shall immediately discontinue our relationship with no additional fee or fine charged.
+</p>
+<br>
+<p>
+    We have the right to contravene the term of advance publishing of a new edition of the Agreement in case such changes are required by law or relate to the introduction of new services, additional functions to the existing Services or any other changes which may neither restrict your rights nor extend your liability limits. In such cases, the changes are made without prior notification of the Users and come into effect immediately.
+</p>
+<br>
+<p>
+    In the case at any moment you no longer agree to those terms or other conditions, you shall immediately discontinue our relationship.
+</p>
+<br>
+<p>
+    In the case you intend to close your User Account, you must understand that you may be liable to us after the termination of the present Agreement for any of your obligations arisen and such which you were liable for before the termination of the present Agreement.
+</p>
+<br>
+<p>
+    The present Agreement shall remain in effect until your User Account is closed in compliance with the procedures required by the System. Altab Productions may terminate the present Agreement for any reason, including breach of the conditions of the present Agreement by you, with such termination immediately taking effect.
+</p>
+<br>
+<p>
+    The present Agreement contains a legally binding provision regarding a dispute resolution procedure, which impacts your legal rights and may be executed by enforcement.
+</p>
+<br>
+<p>
+    Unless the context otherwise requires, the terms used in the text, starting with uppercase letters, shall, herein and in the course of our further contacts and correspondence with you, bear the relevant meanings thereof described in the «Definitions» section. 
+</p>
